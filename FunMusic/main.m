8419ac49b01f0c99a16dd14334f7f8167065e6fe
WangBo <frankiewb@qq.com>
2015-12-25 10:23:19 +0800

    Initial Commit

---
+++ FunMusic/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FunMusic
+//
+//  Created by frankie on 15/12/25.
+//  Copyright © 2015年 Wang Bo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
